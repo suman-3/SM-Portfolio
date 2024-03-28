16745d3da5c9b070efb4cc44cfd1fe968a51445b
Suman Mondal <mondalsumanrock20@gmail.com>
2024-03-28 20:02:20 +0530

    crisp chat Id change

--- src/components/crispBot.js
+++ src/components/crispBot.js
@@ -4,7 +4,7 @@ import { Crisp } from "crisp-sdk-web";
 
 class CrispChat extends Component {
   componentDidMount() {
-    Crisp.configure("d1b39536-a08f-4433-a2aa-5f99f8e3c2b5");
+    Crisp.configure("3df3b395-c634-412d-bd94-4e82e74aee36");
   }
 
   render() {
